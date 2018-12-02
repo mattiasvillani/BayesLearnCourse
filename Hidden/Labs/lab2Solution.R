14fa51f5b6d5251f9c654af2fb3c23e18307922d
Mattias Villani <mattias.villani@gmail.com>
2018-12-02 23:19:31 +0100

    remove

--- Hidden/Labs/lab2Solution.R
+++
@@ -1,6 +0,0 @@
-# Solutions lab 2
-setwd('/home/mv/Dropbox/Teaching/BayesLearn2012/Labs/')
-japanTemp <- read.table('/home/mv/Dropbox/Teaching/BayesLearn2012/Labs/JapanTemp.dat', header = TRUE)
-
-quadReg <- lm(temp ~ time + I(time^2) + I(time^3) + I(time^4) + I(time^5) + I(time^6) + I(time^7) , data = japanTemp)
-
