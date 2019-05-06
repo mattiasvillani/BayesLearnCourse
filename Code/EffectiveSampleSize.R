c406b47ba76f876cd9f11a5daf05650df87d1866
Per Sidén <>
2019-05-06 10:56:37 +0200

    EffectiveSampleSizeExample

---
+++ Code/EffectiveSampleSize.R
@@ -0,0 +1,12 @@
+# Using the coda library to compute the effective sample size
+# Author: Per Sidén
+
+library(coda)
+
+x = rnorm(1000)
+y = x
+for(i in 2:1000){
+  y[i] = .9*y[i-1] + x[i]
+}
+effectiveSize(x)
+effectiveSize(y)
